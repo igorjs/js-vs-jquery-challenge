07a523c88479c87bd61e11569cb7b506f60bb848
Igor J. Santos <igor@getdigital.com.br>
2017-08-15 10:17:57 -0300

    Update Lib
    Signed-off-by: Igor J. Santos <igor@getdigital.com.br>

--- v1/actions.js
+++ v1/actions.js
@@ -3,22 +3,16 @@
  */
 'use strict';
 
-/* Functions */
-var toggle = function (el) {
-    el.style.display = el.style.display === 'none' ? 'block' : 'none';
-};
-
 /* Actions */
-var openClose = false;
-
 $el('.toggle-all').on('click', function () {
-    openClose != openClose;
-
-    $el('.panel .content').forEach(function (el) {
-        el.style.display = openClose ? 'block' : 'none';
-    });
+    var panels = $el('.panel');
+    if (panels.hasClass('open')) {
+        panels.removeClass('open');
+    } else {
+        panels.addClass('open');
+    }
 });
 
 $el('.panel .header').on('click', function () {
-    toggle(this.nextElementSibling);
+    this.parentNode.toggleClass('open');
 });
